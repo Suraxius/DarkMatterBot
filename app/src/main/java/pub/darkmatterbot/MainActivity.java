e26bd26d8458461e3f3cd6e2d9f3a3d6a583395b
Suraxius <sven.master@gmail.com>
2017-10-25 16:41:41 +1300

    -Started work on the main Activity -Did some work on the theme and changed it and the logo to the blue version.

--- app/src/main/java/pub/darkmatterbot/MainActivity.java
+++ app/src/main/java/pub/darkmatterbot/MainActivity.java
@@ -2,12 +2,49 @@ package pub.darkmatterbot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClicked();
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    Toast.makeText(this, "Swiped!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    void buttonClicked() {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show ();
     }
 }
