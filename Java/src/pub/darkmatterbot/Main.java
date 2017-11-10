cc7d479c062446c854833815b370bdf748392a9d
Suraxius <sven.master@gmail.com>
2017-11-11 00:51:48 +1300

    Added basic code to JavaFX version to load the list of servers using libogame

--- Java/src/pub/darkmatterbot/Main.java
+++ Java/src/pub/darkmatterbot/Main.java
@@ -1,30 +1,21 @@
 package pub.darkmatterbot;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.image.Image;
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.net.URL;
+import pub.libogame.*;
 
 public class Main extends Application {
 
-    // description of application
-    //private GridPane root;
-    //private Button btn_insert;
-    //private TextArea ta;
-    //private TextField[] inputs;
-
+    private LibOgame _libOgame;
 
     public static void main(String[] args)
     {
@@ -34,22 +25,36 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        //inputs = new TextField[4];
-        // add listeners for changes to text fields
+        //Initialize the LibOgame instance:
+        try {
+            _libOgame = new LibOgame("https://en.ogame.gameforge.com/");
+        }
+        catch( LibOgameException e ) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
 
+        //Get image file:
+        ClassLoader cl = this.getClass().getClassLoader();
+        URL logo = cl.getResource("images/DarkMatterBotLogo.png");
+
+        //Add UI Elements:
         GridPane root = new GridPane();
+        ListView<String> serverListView = new ListView<String>();
+        root.getChildren().add(serverListView);
+
+	//Add Servers to serverList:
+        String[] serverAL = new String[_libOgame.servers.count()];
+        for( int i = 0; i < serverAL.length; i++ ) serverAL[i] = _libOgame.servers.getName(i);
+        ObservableList<String> oServerAL = FXCollections.observableArrayList(serverAL);
+        serverListView.setItems(oServerAL);
 
         primaryStage.setTitle( "DMB OGame Bot" );
-        primaryStage.getIcons().add(new Image("file:../images/DarkMatterBotLogo.png"));
+        if(logo != null ) primaryStage.getIcons().add(new Image(logo.openStream()));
         primaryStage.setMinHeight( 600 );
         primaryStage.setMaxHeight( 600 );
         primaryStage.setMinWidth( 900 );
         primaryStage.setMaxWidth( 900 );
-        //initiateGrid();
-
-        // Create a new empty JavaFX window of sized 900 x 600px.
-        // containing our grid panes
-
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
